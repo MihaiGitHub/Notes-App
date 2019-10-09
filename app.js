9aa053f96164665922504547160f46ff01f36c22
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-10-08 20:05:33 -0700

    Register the add command using yargs

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const getNotes = require('./notes.js')
 const chalk = require('chalk')
+const yargs = require('yargs')
 const command = process.argv[2]
 
-if(command === 'add'){
-    console.log('adding note')
-} else if (command === 'remove'){
-    console.log('removing note')
-}
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Add command
+// The options object is where we customize how this command should work
+yargs.command({
+    command: 'add', // command name
+    describe: 'Add a new note', // describe what the command is supposed to do
+    handler: function () { // function that gets executed when command gets used
+        console.log('adding a new note')
+    }
+})
+
+// Can view commands that you have access to
+console.log(yargs.argv)
